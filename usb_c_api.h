5e8ef102600f2a335553a328ea07c72d7f93bc3c
Harish Kandekar <61914999+harishkthedeveloper@users.noreply.github.com>
2024-05-09 11:29:46 +0530

    Create usb_c_api.h

---
+++ usb_c_api.h
@@ -0,0 +1,25 @@
+#include <windows.h>
+
+#ifndef FLUTTER_PLUGIN_USB_DETECT_PLUGIN_C_API_H_
+#define FLUTTER_PLUGIN_USB_DETECT_PLUGIN_C_API_H_
+
+#include <flutter_plugin_registrar.h>
+
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
+#else
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllimport)
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+FLUTTER_PLUGIN_EXPORT void UsbDetectPluginCApiRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar);
+
+#if defined(__cplusplus)
+}  // extern "C"
+#endif
+
+#endif  // FLUTTER_PLUGIN_USB_DETECT_PLUGIN_C_API_H_
